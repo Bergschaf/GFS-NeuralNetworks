7dc8a6473884bcfaa9aad16f74cb27da2fe702f7
bergschaf <christian.k@netcom-mail.de>
2023-07-04 20:19:52 +0200

    uuuu

--- slides/scripts/introduction.py
+++ slides/scripts/introduction.py
@@ -6,4 +6,12 @@ class S0(Scene):
         title = Title("Neuronale Netwerke")
         self.add(title)
 
+        self.wait(1)
 
+        img1 = ImageMobject("assets/DALLE.png").scale(0.7).shift(LEFT * 3.85).shift(DOWN * 0.5)
+        img2 = ImageMobject("assets/BERG.png").scale(0.7).next_to(img1, RIGHT * 2.5)
+
+        self.add(img1)
+        self.add(img2)
+
+        self.wait(1)
